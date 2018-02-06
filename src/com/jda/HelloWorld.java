0d88cc12f616340f448c190ae87b23593e9a785d
Tejas Rajpura <Tejas.Rajpura@jda.com>
2018-02-06 15:38:12 +0530

    second commit

--- src/com/jda/HelloWorld.java
+++ src/com/jda/HelloWorld.java
@@ -2,6 +2,6 @@ package com.jda;
  
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world.");
+    System.out.println("This is a test program only.");
   }
 }
